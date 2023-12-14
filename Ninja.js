7989ac4e4a0b8c22016ac29dee70d0dfbffaf6be
nespinoza2000 <113635313+nespinoza2000@users.noreply.github.com>
2023-12-13 21:32:11 -0300

    [TAREA] sensei

--- Ninja.js
+++ Ninja.js
@@ -16,3 +16,21 @@ class Ninja {
         console.log(`${this.name} drank sake and gained 10 health!`);
     }
 }
+
+
+class Sensei extends Ninja {
+    constructor(name) {
+        super(name);
+        this.health = 200;
+        this.strength = 10;
+        this.speed = 10;
+        this.sabiduria = 10;
+    }
+    speakWisdom() {
+        super.drinkSake(); // Aumenta la salud al beber sake para obtener sabiduría
+        console.log("What one programmer can do in one month, two programmers can do in two months.");
+    }
+    drinkSake() {
+        super.drinkSake(); // La acción de beber sake se mantiene igual a la del Ninja
+    }
+}
